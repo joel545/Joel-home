a69bf0a0b78800e0e2699ac984bee920d6074eb3
python-12-1-24 <joelliu848@gmail.com>
2024-12-01 19:01:18 +0800

    12-1-add

--- Unit0202Module.py
+++ Unit0202Module.py
@@ -14,5 +14,11 @@ items("美式咖啡","藝伎咖啡豆","衣索比亞咖啡豆")
 
 
 import Unit0201book as book
+from shop.product import items 
+
 #使用Unit0201booke 模組內的自訂函數
 book.favorite("這才是我活著的目的")
+book.sale("這才是我活著的目的",150,10)
+print('\n------呼叫product 模組之items()--------------------')
+items("冰點","寫作課","自卑與超越","這才是我活著的目的")
+
---
+++ shop/product.py
@@ -0,0 +1,5 @@
+def title() :
+    print("reading time")
+def items(*products):
+    for e in products:
+        print(e)
