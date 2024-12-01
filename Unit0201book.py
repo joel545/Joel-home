8971ac5d90832ad13fc879654624bf5db0d89fdd
python-12-1-24 <joelliu848@gmail.com>
2024-12-01 18:24:41 +0800

    first commit

---
+++ Unit0201book.py
@@ -0,0 +1,13 @@
+#自訂函數
+"""
+def favorite(name):
+    print("您最愛的咖啡是：",name)
+
+def sale(name,price,qty):
+    print("咖啡名稱：%s\n價格：%3d\n數量：%3d\n合計：%4d元"%(name,price,qty,price*qty))
+"""
+
+def favorite(name):
+    print ("我最愛的書本是：",name)
+def sale(name,price,qty):
+    print("書本名稱：%s\n價格：%3d\n數量：%3d\n合計：%4d元"%(name,price,qty,price*qty))
